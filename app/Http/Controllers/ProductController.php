0fc7e933d848d8390c4cc0a514f70d60d4c63c9e
litos-ua <andrzejlitkiewicz@gmail.com>
2024-03-26 17:06:49 +0200

    add resorce Product (api, controller, resource)

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        $products = Product::all();
+        $productsResponce = ProductResource::collection($products);
+        $productsResponce->wrap ('products');
+        return $productsResponce->response();
+    }
+
+    public function indexByCategory($categoryId)
+    {
+        $products = Product::where('category_id', $categoryId)->get();
+        $productsOfCategory = ProductResource::collection($products);
+        $productsOfCategory->wrap('productsOfCategory');
+        return $productsOfCategory->response();
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(string $id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['error' => 'Product not found'], 404);
+        }
+//        return new ProductResource($product);
+        $productResource = new ProductResource($product);
+        $productResource->wrap('product');
+        return $productResource->response();
+    }
+
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +25,10 @@ Route::post('/categories', [CategoryController::class, 'store']);
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
 Route::put('/categories/{id}', [CategoryController::class, 'update']);
 Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/category/{categoryId}', [ProductController::class, 'indexByCategory']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
