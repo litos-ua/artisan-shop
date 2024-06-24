67b7a07d1ea8ac6d471e6fa3be4966d9528653b2
litos-ua <andrzejlitkiewicz@gmail.com>
2024-06-24 11:32:55 +0200

    Add message interface. Create Message and ChatInterface components. Add httpMessageClient. AdminPanel has been modified.

--- src/pages/Admin/Messages/Messages.jsx
+++ src/pages/Admin/Messages/Messages.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { List, Datagrid, TextField, DateField, useDataProvider, useNotify, useRefresh, SimpleForm,
+         TextInput, Create
+} from 'react-admin';
 
-const Messages = () => {
-    return <div>Messages Component</div>;
+export const MessageList = (props) => {
+    const [messages, setMessages] = useState([]);
+    const dataProvider = useDataProvider();
+    const notify = useNotify();
+    const refresh = useRefresh();
+
+    useEffect(() => {
+        dataProvider.getList('messages', { userId: props.userId })
+            .then(({ data }) => setMessages(data))
+            .catch(error => notify(`Error: ${error.message}`, 'warning'));
+    }, [dataProvider, notify, refresh, props.userId]);
+
+    return (
+        <List {...props}>
+            <Datagrid>
+                <TextField source="sender.name" label="Sender" />
+                <TextField source="receiver.name" label="Receiver" />
+                <TextField source="message" />
+                <DateField source="timestamp" />
+            </Datagrid>
+        </List>
+    );
 };
 
-export default Messages;
+export const MessageCreate = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput source="receiver_id" />
+            <TextInput source="message" multiline />
+        </SimpleForm>
+    </Create>
+);
