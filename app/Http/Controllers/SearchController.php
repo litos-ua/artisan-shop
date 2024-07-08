165adf5753ac857939d0ce0ba6fbde411c110279
litos-ua <andrzejlitkiewicz@gmail.com>
2024-07-08 15:43:37 +0300

    Delete some comment code.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -25,20 +25,4 @@ class SearchController extends Controller
 }
 
 
-//namespace App\Http\Controllers;
-//
-//use Illuminate\Http\Request;
-//use App\Http\Resources\ProductResource;
-//use App\Models\Product;
-//class SearchController extends Controller
-//{
-//    public function searchProducts($search)
-//    {
-//        $products = Product::where('name', 'like', "%$search%")->get();
-//
-//        $productsResponse = ProductResource::collection($products);
-//        $productsResponse->wrap('productsBySearchParam');
-//
-//        return $productsResponse->response();
-//    }
-//}
+
